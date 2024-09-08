5cd391780dd5ddaad958d70116cc3585e7d5ebe5
Lang (Ron) Chen <Ronstuckathome@gmail.com>
2024-09-08 22:04:19 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="JXAutoML",  # Replace with your package name
-    version="0.4.1",
+    version="0.4.2",
     author="Lang Chen",
     author_email="ronchen6666@gmail.com",
     description="Advanced HyperParameter Tuning",
